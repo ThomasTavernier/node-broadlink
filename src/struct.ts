59a6870babd8d74e02e5684f14feebbef80ebe50
Thomas TAVERNIER <thomas@tavernier.dev>
2023-01-01 17:28:19 +0100

    fix: struct unpack & unpack_from

--- src/struct.ts
+++ src/struct.ts
@@ -1,5 +1,8 @@
 import struct from '@aksel/structjs';
 
+const bufferToArrayBuffer = (buffer: Buffer): ArrayBuffer =>
+  buffer.buffer.slice(buffer.byteOffset, buffer.byteOffset + buffer.byteLength);
+
 export default (
   format: string,
 ): {
@@ -9,13 +12,14 @@ export default (
   pack: (...values: number[]) => Buffer;
 } => {
   const s = struct(format);
+
   return {
-    unpack_from: (buffer, offs) => s.unpack_from(buffer.buffer, offs),
+    unpack_from: (buffer, offs) => s.unpack_from(bufferToArrayBuffer(buffer), offs),
     pack_into: (buffer, offs, ...values): Buffer => {
       s.pack_into(buffer.buffer, offs, ...values);
       return buffer;
     },
-    unpack: (buffer) => s.unpack(buffer.buffer),
+    unpack: (buffer) => s.unpack(bufferToArrayBuffer(buffer)),
     pack: (...values) => Buffer.from(s.pack(...values)),
   };
 };
